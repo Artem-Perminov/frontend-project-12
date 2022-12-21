3fde4cb7baab2c1bcc50d6f31e6cdcc8541f8ede
Artem Perminov <perminov.artem@eurostavka.ru>
2022-12-21 23:59:39 +0300

    feat: add routes_header_login_and_notfound_pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,17 @@
-const App = () => <div>Hello, people!</div>;
+import { Routes, Route } from 'react-router-dom';
+import Login from './components/pages/Login';
+import Notfound from './components/pages/Notfound';
+import Header from './components/Header';
+
+const App = () => (
+  <>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="*" element={<Notfound />} />
+    </Routes>
+  </>
+);
 
 export default App;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => <Link to="/">New Slack!</Link>;
+
+export default Header;
